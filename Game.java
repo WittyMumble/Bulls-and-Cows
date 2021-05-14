92dd30c8e07f35e9f9c83db912c008467b96d6e2
Feb <BeliaPo@whs.school.nz>
2021-05-14 14:47:23 +1200

    Initial sharing of project

---
+++ Game.java
@@ -0,0 +1,22 @@
+/**
+ * A simple game of Bulls and Cows
+ *
+ * @author Feb Beliaeva
+ * @version May-13-2021
+ */
+
+import java.util.Scanner; //Scans for user input
+import java.util.Random; //
+import java.io.File;
+import java.io.IOException;
+public class Game
+{
+    int cows;
+    int bulls;
+    int numberSize;
+    String number[];
+    public Game()
+    {
+        
+    }
+}
