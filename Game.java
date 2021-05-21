6e260e063d50a9b8d3918903daacce78247fe9e4
WittyMumble <82478935+WittyMumble@users.noreply.github.com>
2021-05-21 15:15:43 +1200

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@
  * A simple game of Bulls and Cows
  *
  * @author Feb Beliaeva
- * @version May-13-2021
+ * @version May-20-2021
  */
 
 import java.util.Scanner; //Scans for user input
@@ -14,10 +14,21 @@ public class Game
     String filename = "intro.txt"; //stores name of file containing introductory text
     int bulls;
     int cows;
-    int numberSize; //keeps track of the number of digits of the number being guessed
-    String number[];
+    double min;
+    double max;
+    final int MAXDIGITS = 5;
+    int number[] = new int[MAXDIGITS];
+    int digit = 0; //keeps track of place in array for the initial generation
+    //keeps track of the number of digits of the number being guesses
+    //initializing the number generator
+    int RandomNumber(int min,int max){
+            int x = (int)Math.floor((Math.random()*((max-min)+1))+min); 
+            return x;
+        }//I got this from a java tutorial online
+    //main block
     public Game()
     {
+        System.out.println('\u000c'); //clears terminal
         //Reading intro text from a file. storing it in a file is easier since it takes up less space and is easier to edit.
         File theFile=new File(filename);
         try {
@@ -30,7 +41,17 @@ public class Game
             e.printStackTrace();
             System.out.println("Something is wrong with a file needed for this program");
         }
+        //scanner for the actual game
+        Scanner input = new Scanner (System.in);
         
-        
+        //Generating the Number
+        System.out.println("How many digits do you want the number to have?");
+        int numberSize = input.nextInt();
+        if(numberSize > MAXDIGITS) System.out.println("for the sake of your own sanity, please don't pick such a large number");
+        while (digit < numberSize) {
+            number[digit] = RandomNumber(0,9);
+            digit++;
+        }
     }
 }
+
